e11ec055be23dbbe50c94b895928072ef2f0ef85
ISABELLA ALVES DA SILVA LIMA TAVARES <isabella.asltavares@senacsp.edu.br>
2024-08-13 15:33:48 -0300

    bug da imagem corrigido

--- index.php
+++ index.php
@@ -4,35 +4,52 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Galeria de Imagens</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./assets./css./style.css">
     <style>
-        .gallery{
-            display: flex;
+ 
+        .gallery {
+            display:flex;
             flex-wrap: wrap;
+            justify-content: space-between;
+            padding-left: 20px;
+            background-color: rgba(255, 255, 255, 0.507);
+            gap:32px;
+            padding-bottom: 40px;
         }
-        .gallery img{
-            width: 19%;
+        .gallery img {
+            width: 29%;
             margin: 0.5%;
             transition: transform 0.2s;
+            background-color: white;
+            padding: 8px;
         }
-        .gallery img:hover{
+        .gallery img:hover {
             transform: scale(1.5);
         }
     </style>
-    <body>
-        <h1>Galeria de Imagens</h1>
-        <form action="upload.php" method="post" enctype="multipart/form-data">  
-        </form>
-        <div class="gallery">
-            <?php
-            $files = glob("uploads/*.*");
-            foreach($files as $file) {
-                echo '<img scr="' . $file . '" alt=Imagem"';
-            }
-            ?>
-        </div>
-        
-    </body>
 </head>
-
+<body>
+    <h1>Galeria de Imagens</h1>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="image" accept="image/*">
+        <button type="submit">Enviar Imagem</button>
+    </form>
+    <div class="gallery">
+    <?php
+       
+        $files = glob("uploads/*.*");
+        // Utiliza a função glob para obter uma lista de todos os arquivos na pasta "uploads" com qualquer extensão
+       
+        foreach($files as $file) {
+            // Itera sobre cada arquivo na lista de arquivos obtida
+       
+            echo '<img src="' . $file . '" alt="Imagem">';
+            // Exibe cada arquivo como uma imagem HTML, com o atributo "src" apontando para o caminho do arquivo
+            // O atributo "alt" fornece um texto alternativo "Imagem" caso a imagem não possa ser carregada
+        }
+       
+        ?>
+       
+    </div>
+</body>
 </html>
